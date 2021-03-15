824084f4404985e00f970133dc556921add438d2
QYIF <499600328@qq.com>
2021-03-15 17:32:15 +0800

    :rocket:add

--- src/main/java/experiment2/dqy/Car.java
+++
@@ -1,5 +0,0 @@
-package experiment2.dqy;
-
-public class Car {
-
-}
---
+++ src/main/java/experiment2/dqy/Model/Car.java
@@ -0,0 +1,42 @@
+package experiment2.dqy.Model;
+
+import java.util.Date;
+
+public class Car {
+    private String no;
+    private String name;
+    private Date enterTime;
+    private Date leavingTime;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getEnterTime() {
+        return enterTime;
+    }
+
+    public void setEnterTime(Date enterTime) {
+        this.enterTime = enterTime;
+    }
+
+    public Date getLeavingTime() {
+        return leavingTime;
+    }
+
+    public void setLeavingTime(Date leavingTime) {
+        this.leavingTime = leavingTime;
+    }
+}
