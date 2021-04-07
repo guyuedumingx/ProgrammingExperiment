17192020001a54896dbe061c750d8ca30ffcde00
60rzvvbj <1808078515@qq.com>
2021-04-07 14:42:53 +0800

    :rocket: add io

--- src/main/java/experiment3/yhicxu/test/TestHuffman.java
+++ src/main/java/experiment3/yhicxu/test/TestHuffman.java
@@ -1,7 +1,12 @@
 package experiment3.yhicxu.test;
 
-public class TestHuffman {
-    public static void main(String[] args) {
+import experiment3.yhicxu.util.IOUtil;
 
+import java.io.IOException;
+
+public class TestHuffman {
+    public static void main(String[] args) throws IOException {
+        String s = IOUtil.readFileText("experiment3/Demo.txt");
+        System.out.println(s);
     }
 }
--- src/main/java/experiment3/yhicxu/util/IOUtil.java
+++ src/main/java/experiment3/yhicxu/util/IOUtil.java
@@ -1,8 +1,35 @@
 package experiment3.yhicxu.util;
 
+import experiment3.yhicxu.test.TestHuffman;
+
+import java.io.*;
+import java.net.URL;
+
 public class IOUtil {
-    public static String readFile(String path) {
-        return null;
+    public static String readFileText(String path) {
+        URL resource = TestHuffman.class.getClassLoader().getResource(path);
+        path = resource.getPath();
+        Reader reader = null;
+        char[] chars = new char[1024];
+        int len;
+        StringBuilder res = new StringBuilder();
+        try {
+            reader = new FileReader(path);
+            while ((len = reader.read(chars)) != -1) {
+                res.append(new String(chars, 0, len));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return res.toString();
     }
 
 }
