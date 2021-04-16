350b19e64551eca436a60923d01b1cde6ce5fec9
guyuedumingx <guyuedumingx@gmail.com>
2021-04-17 02:27:15 +0800

    :alien: fix Resource method

--- src/main/java/util/Resource.java
+++ src/main/java/util/Resource.java
@@ -5,6 +5,7 @@ import java.net.URL;
 public class Resource {
 
     public static String get(String name) {
+        name = "../" + name;
         URL resource = new Resource().getClass().getResource(name);
         return resource.getFile();
     }
