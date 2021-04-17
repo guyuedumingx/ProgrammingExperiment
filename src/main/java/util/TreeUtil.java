77835ce5f8322986151e8c2ccbab36cac3557db3
60rzvvbj <1808078515@qq.com>
2021-04-17 14:29:44 +0800

    :rocket: add printTree

--- src/main/java/util/TreeUtil.java
+++ src/main/java/util/TreeUtil.java
@@ -1,7 +1,77 @@
 package util;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class TreeUtil {
     public void buildXmind() {
 
+    }
+
+    public void printTree(TreeNode root) {
+        LinkedList<TreeNode> allNode = new LinkedList<>();
+        HashMap<TreeNode, Integer> nodeHeight = new HashMap<>();
+        LinkedList<TreeNode> leafNode = new LinkedList<>();
+        showTreeDfs(allNode, nodeHeight, leafNode, 0, root);
+        HashMap<TreeNode, Integer> nodeX = new HashMap<>();
+        HashMap<TreeNode, Integer> nodeY = new HashMap<>();
+        int maxHeight = 0;
+        for (TreeNode node : nodeHeight.keySet()) {
+            maxHeight = Math.max(nodeHeight.get(node), maxHeight);
+        }
+        int width = maxHeight + maxHeight / 2;
+        char[][] chart = new char[maxHeight * 2 + 2][width * (leafNode.size() + 1)];
+        for (char[] chars : chart) {
+            Arrays.fill(chars, ' ');
+        }
+        int x = width;
+        for (TreeNode node : leafNode) {
+            nodeX.put(node, x);
+            x += width;
+        }
+        for (int y = maxHeight; y >= 0; y--) {
+            for (TreeNode node : allNode) {
+                if (nodeHeight.get(node) == y) {
+                    nodeY.put(node, 2 * y);
+                    if (node.getLeft() != null || node.getLeft() != null) {
+                        nodeX.put(node, (nodeX.get(node.getLeft()) + nodeX.get(node.getRight())) / 2);
+                    }
+                }
+            }
+        }
+        for (TreeNode node : allNode) {
+            String text = node.getData().replaceAll("\r", "\\r").replaceAll("\n", "\\n").replaceAll(" ", "凵");
+            setChar(chart, text, nodeX.get(node), nodeY.get(node));
+            chart[(nodeY.get(node) + nodeY.get(node.getLeft())) / 2][(nodeX.get(node) + nodeX.get(node.getLeft())) / 2] = '/';
+            chart[(nodeY.get(node) + nodeY.get(node.getRight())) / 2][(nodeX.get(node) + nodeX.get(node.getRight())) / 2 + 1] = '\\';
+        }
+        for (int i = 0; i < chart.length; i++) {
+            for (int j = 0; j < chart[i].length; j++) {
+                System.out.print(chart[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private void setChar(char[][] chart, String str, int x, int y) {
+        char[] chars = str.toCharArray();
+        int len = chars.length;
+        int start = x - len / 2;
+        int index = 0;
+        for (int i = start; index < chars.length; i++) {
+            chart[y][i] = chars[index++];
+        }
+    }
+
+    private void showTreeDfs(LinkedList<TreeNode> allNode, HashMap<TreeNode, Integer> nodeHeight, LinkedList<TreeNode> leafNode, int nowHeight, TreeNode node) {
+        allNode.add(node);
+        nodeHeight.put(node, nowHeight);
+        if (node.getLeft() == null && node.getRight() == null) {
+            leafNode.add(node);
+        } else {
+            showTreeDfs(allNode, nodeHeight, leafNode, nowHeight + 1, node.getLeft());
+            showTreeDfs(allNode, nodeHeight, leafNode, nowHeight + 1, node.getRight());
+        }
     }
 }
