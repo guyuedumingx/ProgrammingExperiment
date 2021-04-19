83a6d2663f0dec36c77e12af903f7144f299520c
QYif <499600328@qq.com>
2021-04-19 10:22:26 +0800

    :alien:updated

--- src/main/java/experiment3/dqy/HuffmanTree.java
+++ src/main/java/experiment3/dqy/HuffmanTree.java
@@ -3,25 +3,46 @@ import util.Resource;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class HuffmanTree {
     HashMap<Character, Integer> cnt = new HashMap<Character, Integer>();
-    PriorityQueue<Integer> helper = new PriorityQueue<Integer>();
-            public String getFrequency() throws IOException {
-                String url = Resource.get("experiment3/Demo.txt");
-                System.out.println();
-                FileReader fileReader = new FileReader(url);
-                BufferedReader br = new BufferedReader(fileReader);
-                String s, ans = "";
-                while ((s = br.readLine()) != null) {
-                    ans = s.toString();
-                    System.out.println(ans);
+    HashMap<String, Character> mp = new HashMap<String, Character>();
+    PriorityQueue<pair> helper = new PriorityQueue<pair>((o1, o2) -> o1.fre - o2.fre);
+    public void getFrequency() throws IOException {
+        String url = Resource.get("experiment3/Demo.txt");
+        System.out.println();
+        FileReader fileReader = new FileReader(url);
+        BufferedReader br = new BufferedReader(fileReader);
+        char read;
+        while ((read = (char)br.read()) != -1) {
+            if (cnt.containsKey(read)) {
+                int cur = cnt.get(read);
+                cnt.replace(read, cur + 1);
+            } else {
+                cnt.put(read, 1);
+            }
         }
-        return ans;
     }
 
-    public static void main(String[] args) throws IOException {
-        String s = new HuffmanTree().getFrequency();
+//    public void makeTree() {
+//        for (Map.Entry entry:cnt.entrySet()) {
+//            helper.add(new pair(entry.getKey(), entry.getValue()));
+//        }
+//        while (!helper.isEmpty()) {
+//
+//        }
+//    }
+
+}
+
+class pair{
+    Character key;
+    Integer fre;
+    pair(Object k, Object f) {
+        this.key = (Character) k;
+        this.fre = (Integer) f;
     }
 }
