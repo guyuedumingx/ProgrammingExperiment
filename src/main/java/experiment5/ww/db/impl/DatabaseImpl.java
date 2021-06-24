79709f60853ba032f4692e2e21662e7049dce9b5
guyuedumingx <guyuedumingx@gmail.com>
2021-06-24 16:56:49 +0800

    update exp5 to file

--- src/main/java/experiment5/ww/db/impl/DatabaseImpl.java
+++ src/main/java/experiment5/ww/db/impl/DatabaseImpl.java
@@ -1,14 +1,16 @@
 package experiment5.ww.db.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import experiment5.ww.db.Database;
 import experiment5.ww.db.DBNode;
-import util.graphutil.Graph;
-import util.graphutil.GraphEdge;
-import util.graphutil.GraphNode;
-import util.graphutil.GraphRoute;
 
+import java.io.*;
+import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  * 数据库实现类
@@ -18,11 +20,64 @@ import java.util.List;
  */
 public class DatabaseImpl<T extends DBNode> implements Database<T> {
     private Node<T> head = new Node<T>();
+    private ObjectMapper mapper = new ObjectMapper();
+    private String file;
+    private FileReader reader;
+    private FileWriter writer;
+
+    private void save() {
+        List<T> ts = selectAll();
+        try {
+            String s = mapper.writeValueAsString(ts);
+            close();
+            writer.write(s);
+            writer.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void close() {
+        try {
+            writer.write("");
+            writer.flush();
+            writer.close();
+            writer = new FileWriter(file);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void read() {
+        char[] buf = new char[1024];
+        StringBuilder sb = new StringBuilder();
+        try {
+            while (reader.read(buf) != -1) {
+                sb.append(buf.toString());
+            }
+            T t = mapper.readValue(sb.toString(), (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+            insert(t);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DatabaseImpl(String file) {
+        this.file = file;
+        try {
+            reader = new FileReader(file);
+            writer = new FileWriter(file);
+            read();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void insert(T t) {
         Node<T> node = new Node<T>(t);
         node.setNext(head.getNext());
         head.setNext(node);
+        save();
     }
 
     public boolean deleteByNo(String no) {
@@ -49,7 +104,6 @@ public class DatabaseImpl<T extends DBNode> implements Database<T> {
         return list;
     }
 
-
     public void update(T t) {
        Node<T> cur = head.getNext();
        while (cur != null) {
@@ -58,6 +112,7 @@ public class DatabaseImpl<T extends DBNode> implements Database<T> {
             }
             cur = cur.getNext();
        }
+       save();
     }
 
     public T selectByNo(String no) {
--- src/main/java/experiment5/ww/util/DbUtil.java
+++ src/main/java/experiment5/ww/util/DbUtil.java
@@ -3,6 +3,9 @@ package experiment5.ww.util;
 import experiment5.ww.db.Database;
 import experiment5.ww.db.impl.DatabaseImpl;
 import experiment5.ww.pojo.Card;
+import util.Resource;
+
+import java.net.URL;
 
 /**
  * 数据库工具类
@@ -10,7 +13,8 @@ import experiment5.ww.pojo.Card;
  * @date 2021/5/19 17:08
  */
 public class DbUtil {
-    private static Database<Card> cardDB = new DatabaseImpl();
+    private static String file = "experiment5/db.json";
+    private static Database<Card> cardDB = new DatabaseImpl(Resource.get(file));
 
     public static Database<Card> getCardDB() {
         return cardDB;
